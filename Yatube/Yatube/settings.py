a6849c91c197a8021202fb7d87e2f24dbccf680c
rbexter <bekhter.ru@gmail.com>
2025-02-15 18:17:15 +0300

    Take secret key to env file

--- Yatube/Yatube/settings.py
+++ Yatube/Yatube/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1si8=or@qszmgdoiu6sxw(o(bpnq1i%=++n$j7fb0o76$&p=xi'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,14 +76,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Yatube.wsgi.application'
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
