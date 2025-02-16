3f39e96ceaf33ead9daf5921eb0279d989d48bef
rbexter <bekhter.ru@gmail.com>
2025-02-16 19:39:46 +0300

    Add nginx container

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.9
 RUN mkdir /app
 COPY requirements.txt /app
+RUN pip install --upgrade pip
 RUN pip install -r /app/requirements.txt --no-cache-dir
 COPY Yatube/ /app
 WORKDIR /app
--- Yatube/Yatube/settings.py
+++ Yatube/Yatube/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
