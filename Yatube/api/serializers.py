59e0ec81d6d3483f2b211c87a96b6d9a28e6f929
rbexter <bekhter.ru@gmail.com>
2025-03-01 14:03:10 +0300

    Edit serializers

--- Yatube/api/serializers.py
+++ Yatube/api/serializers.py
@@ -6,7 +6,6 @@ from posts.models import Comment, Group, Post
 class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
-        #fields = ('id', 'title', 'slug', 'description',)
         fields = ('title',)
         model = Group
 
@@ -16,8 +15,6 @@ class PostSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(
         read_only=True, default=serializers.CurrentUserDefault())
     group = serializers.StringRelatedField(read_only=True)
-    # group = GroupSerializer()
-    # symbol_quantity = serializers.SerializerMethodField()
     publication_date = serializers.DateTimeField(
         source='pub_date', read_only=True
     )
